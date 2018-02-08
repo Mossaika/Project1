04e14090441da5d1a6dc00db168dae0dbf296c14
Joses <36030209+Mossaika@users.noreply.github.com>
2018-02-08 15:39:27 +0700

    New entity

---
+++ src/com/sales/entity/Transaction.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sales.entity;
+
+/**
+ *
+ * @author Developer
+ */
+public class Transaction {
+
+    private int id;
+    private int payment;
+
+    public Transaction(int id, int payment) {
+        this.id = id;
+        this.payment = payment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPayment() {
+        return payment;
+    }
+
+    public void setPayment(int payment) {
+        this.payment = payment;
+    }
+
+}
